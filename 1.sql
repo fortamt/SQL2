3de51b61338bec278f7d39cd04ea57d6e2bd8fb6
fortamt <90449535+fortamt@users.noreply.github.com>
2022-02-08 15:10:04 +0200

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,13 +1,13 @@
 ALTER TABLE developers
 ADD COLUMN salary INT;
 
-UPDATE developers SET salary = 2400 WHERE developer_id = 1;
-UPDATE developers SET salary = 2800 WHERE developer_id = 2;
-UPDATE developers SET salary = 7500 WHERE developer_id = 3;
-UPDATE developers SET salary = 6500 WHERE developer_id = 4;
-UPDATE developers SET salary = 9800 WHERE developer_id = 5;
-UPDATE developers SET salary = 3100 WHERE developer_id = 6;
-UPDATE developers SET salary = 4600 WHERE developer_id = 7;
-UPDATE developers SET salary = 6300 WHERE developer_id = 8;
-UPDATE developers SET salary = 2900 WHERE developer_id = 9;
-UPDATE developers SET salary = 900 WHERE developer_id = 10;
+UPDATE developers SET salary = 2400 WHERE id = 1;
+UPDATE developers SET salary = 2800 WHERE id = 2;
+UPDATE developers SET salary = 7500 WHERE id = 3;
+UPDATE developers SET salary = 6500 WHERE id = 4;
+UPDATE developers SET salary = 9800 WHERE id = 5;
+UPDATE developers SET salary = 3100 WHERE id = 6;
+UPDATE developers SET salary = 4600 WHERE id = 7;
+UPDATE developers SET salary = 6300 WHERE id = 8;
+UPDATE developers SET salary = 2900 WHERE id = 9;
+UPDATE developers SET salary = 900 WHERE id = 10;
