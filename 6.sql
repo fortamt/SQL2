2ad3d453eba3f200b17e127e8854ac03e1342be1
fortamt <90449535+fortamt@users.noreply.github.com>
2022-02-08 15:11:19 +0200

    Update 6.sql

--- 6.sql
+++ 6.sql
@@ -1,5 +1,5 @@
 SELECT project_name, AVG(salary) FROM projects p
-JOIN developers_projects_relation dpr ON p.project_id = dpr.project_id
-JOIN developers d ON dpr.developer_id = d.developer_id
+JOIN developers_projects_relation dpr ON p.id = dpr.project_id
+JOIN developers d ON dpr.developer_id = d.id
 WHERE p.cost = (SELECT MIN(cost) FROM projects)
 GROUP BY project_name;
