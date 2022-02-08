49b3c939a414133c830c229f6c4dc855f1142fef
fortamt <90449535+fortamt@users.noreply.github.com>
2022-02-08 15:10:37 +0200

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,3 +1,3 @@
-SELECT SUM(salary) FROM developers d JOIN developers_skills_relation dsr ON d.developer_id = dsr.developer_id
-JOIN skills s ON dsr.skill_id = s.skill_id
+SELECT SUM(salary) FROM developers d JOIN developers_skills_relation dsr ON d.id = dsr.developer_id
+JOIN skills s ON dsr.skill_id = s.id
 WHERE s.language = 'Java';
