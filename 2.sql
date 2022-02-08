adcf4c4a26e3925a745752d3b0f99ff1a97e6472
fortamt <90449535+fortamt@users.noreply.github.com>
2022-02-08 15:10:23 +0200

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,5 +1,5 @@
-SELECT project_name, SUM(salary) FROM projects p JOIN developers_projects_relation dpr ON p.project_id = dpr.project_id
-JOIN developers d ON dpr.developer_id = d.developer_id
+SELECT project_name, SUM(salary) FROM projects p JOIN developers_projects_relation dpr ON p.id = dpr.project_id
+JOIN developers d ON dpr.developer_id = d.id
 GROUP BY project_name
 ORDER BY 2 DESC
 LIMIT 1;
