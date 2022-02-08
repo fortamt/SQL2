fd7d7c4df9f3ccc8451611319a424098585c832a
fortamt <90449535+fortamt@users.noreply.github.com>
2022-02-08 15:10:51 +0200

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -2,6 +2,6 @@ ALTER TABLE projects
 ADD COLUMN cost INT;
 UPDATE projects p1
 SET cost = (SELECT SUM(d.salary) FROM projects p2
-			JOIN developers_projects_relation dpl ON p2.project_id = dpl.project_id
-			JOIN developers d ON d.developer_id = dpl.developer_id
-WHERE p1.project_id = p2.project_id);
+			JOIN developers_projects_relation dpl ON p2.id = dpl.project_id
+			JOIN developers d ON d.id = dpl.developer_id
+WHERE p1.id = p2.id);
